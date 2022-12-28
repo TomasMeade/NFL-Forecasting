66f746b22e9746228538e1e857497a167ad84307
Tomas Meade <tomasmiagh@gmail.com>
2022-12-28 14:08:23 -0800

    Update data

--- NFL_data/read_data.py
+++ NFL_data/read_data.py
@@ -7,28 +7,9 @@ Created on Fri Dec 16 15:20:35 2022
 """
 
 import pandas as pd
-import os
-import pickle
 
 # All data comes from FantasyPros execept the average draft position data
 # which comes from fantasyData
-
-def read_data(folder):
-    
-    entries = os.listdir(folder)
-    
-    df_dic = {}
-    
-    for entry in entries:
-        name = entry.split('.')[0]
-        df_dic[name] = pd.read_csv(folder + entry, encoding= 'unicode_escape')
-    
-    return df_dic
-        
-
-misc_df_dic = read_data('data/misc/')
-with open('misc_df_dic.pickle', 'wb') as handle:
-    pickle.dump(misc_df_dic, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 all_stats_df = pd.read_csv('data/all_stats/all_stats.csv')
